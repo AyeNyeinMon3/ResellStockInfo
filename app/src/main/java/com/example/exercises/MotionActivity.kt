2a2fc0939127f8817ccf9ee16dc140936df4fa09
AyeNyeinMon3 <an.mon0003@gmail.com>
2023-09-13 17:32:03 +0630

    fix

--- app/src/main/java/com/example/exercises/MotionActivity.kt
+++ app/src/main/java/com/example/exercises/MotionActivity.kt
@@ -7,6 +7,7 @@ import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import com.example.exercises.databinding.ActivityMotionBinding
+import com.google.android.material.button.MaterialButton
 
 class MotionActivity:AppCompatActivity() {
 
@@ -18,16 +19,16 @@ class MotionActivity:AppCompatActivity() {
         binding = ActivityMotionBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val btnEditMortgagePrice = binding.includeMortgage.btnEditMortgagePrice
 
-        binding.includePruchase.btnEditPurchasePrice.setOnTouchListener { _, event ->
-            if (event.action == MotionEvent.ACTION_UP) {
-                btnEditMortgagePrice.isEnabled = true
-            } else if (event.action == MotionEvent.ACTION_DOWN) {
-                btnEditMortgagePrice.isEnabled = false
+        val btnList = listOf<MaterialButton>(binding.includePurchase.btnEditPurchasePrice,binding.includeMortgage.btnEditMortgagePrice)
+
+
+        binding.includePurchase.btnEditPurchasePrice.setOnClickListener {
+            btnList.forEach {view->
+
+                view.isEnabled = btnList.any { !it.isVisible }
+
             }
-
-            false
         }
 
 
